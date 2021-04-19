06e0958c297298b8402485ac9eb442e1fac4adc4
Taufan <taufanderazor@gmail.com>
2021-04-19 10:44:48 +0700

    Initial commit

---
+++ app/src/main/java/com/topanlabs/filmtopan/detail/DetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.topanlabs.filmtopan.detail
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by taufan-mft on 4/19/2021.
+ */
+class DetailViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/topanlabs/filmtopan/list/ListViewModel.kt
@@ -0,0 +1,9 @@
+package com.topanlabs.filmtopan.list
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by taufan-mft on 4/19/2021.
+ */
+class ListViewModel : ViewModel() {
+}

a0a7b44d0e424677967dc8cf7281dd381bfefbfe
Taufan <taufanderazor@gmail.com>
2021-04-19 15:09:23 +0700

    insert factory

---
+++ app/src/main/java/com/topanlabs/filmtopan/data/FilmFactory.kt
@@ -0,0 +1,128 @@
+package com.topanlabs.filmtopan.data
+
+/**
+ * Created by taufan-mft on 4/19/2021.
+ */
+class FilmFactory {
+    val name = listOf(
+            "A Star is Born",
+            "Alita: Battle Angel",
+            "Aquaman",
+            "Bohemian",
+            "Cold Pursuit",
+            "Creed",
+            "The Crimes of Grindelwald",
+            "Glass",
+            "How to Train Your Dragon",
+            "Infinity War"
+    )
+
+    val year = listOf(
+            "2018",
+            "2019",
+            "2018",
+            "2018",
+            "2019",
+            "2015",
+            "2018",
+            "2019",
+            "2010",
+            "2018",
+    )
+
+    val userScore = listOf(
+            75,
+            72,
+            69,
+            80,
+            57,
+            74,
+            69,
+            67,
+            78,
+            83
+
+    )
+
+    val shortDesc = listOf(
+            "Seasoned musician Jackson Maine discovers — and falls in love with — struggling artist Ally. She has just about given up on her dream to make it big as a singer — until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.",
+            "When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.",
+            "Once home to the most advanced civilization on Earth, Atlantis is now an underwater kingdom ruled by the power-hungry King Orm. With a vast army at his disposal, Orm plans to conquer the remaining oceanic people and then the surface world. Standing in his way is Arthur Curry, Orm's half-human, half-Atlantean brother and true heir to the throne.",
+            "Singer Freddie Mercury, guitarist Brian May, drummer Roger Taylor and bass guitarist John Deacon take the music world by storm when they form the rock 'n' roll band Queen in 1970. Hit songs become instant classics. When Mercury's increasingly wild lifestyle starts to spiral out of control, Queen soon faces its greatest challenge yet – finding a way to keep the band together amid the success and excess.",
+            "The quiet family life of Nels Coxman, a snowplow driver, is upended after his son's murder. Nels begins a vengeful hunt for Viking, the drug lord he holds responsible for the killing, eliminating Viking's associates one by one. As Nels draws closer to Viking, his actions bring even more unexpected and violent consequences, as he proves that revenge is all in the execution.",
+            "The former World Heavyweight Champion Rocky Balboa serves as a trainer and mentor to Adonis Johnson, the son of his late friend and former rival Apollo Creed.",
+            "Gellert Grindelwald has escaped imprisonment and has begun gathering followers to his cause—elevating wizards above all non-magical beings. The only one capable of putting a stop to him is the wizard he once called his closest friend, Albus Dumbledore. However, Dumbledore will need to seek help from the wizard who had thwarted Grindelwald once before, his former student Newt Scamander, who agrees to help, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world.",
+            "In a series of escalating encounters, former security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.",
+            "As the son of a Viking leader on the cusp of manhood, shy Hiccup Horrendous Haddock III faces a rite of passage: he must kill a dragon to prove his warrior mettle. But after downing a feared dragon, he realizes that he no longer wants to destroy it, and instead befriends the beast – which he names Toothless – much to the chagrin of his warrior father",
+            "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain."
+    )
+
+    val language = "English"
+
+    val status = "Released"
+
+    val budget = listOf(
+            "$36,000,000",
+            "$170,000,000",
+            "$160,000,000",
+            "$52,000,000",
+            "$60,000,000",
+            "$37,000,000",
+            "$200,000,000",
+            "$20,000,000",
+            "$165,000,000",
+            "$300,000,000"
+    )
+
+    val income = listOf(
+            "$433,888,866",
+            "$404,852,543",
+            "$1,148,461,807",
+            "$894,027,543",
+            "$76,419,755",
+            "$173,567,581",
+            "$653,355,901",
+            "$246,941,965",
+            "$494,878,759",
+            "$2,046,239,637"
+    )
+
+    val ageRating = listOf(
+            "R",
+            "PG-13",
+            "PG-13",
+            "PG-13",
+            "R",
+            "PG-13",
+            "PG-13",
+            "PG-13",
+            "PG",
+            "PG-13"
+    )
+
+    val tags = listOf(
+            "Drama, Romance, Music",
+            "Action, Science Fiction, Adventure",
+            "Action, Adventure, Fantasy",
+            "Music, Drama, History",
+            "Action, Crime, Thriller",
+            "Drama",
+            "Adventure, Fantasy, Drama",
+            "Thriller, Drama, Science Fiction",
+            "Fantasy, Adventure, Animation, Family",
+            "Adventure, Action, Science Fiction"
+    )
+
+    val runtime = listOf(
+            "2h 16m",
+            "2h 2m",
+            "2h 23m",
+            "2h 15m",
+            "1h 59m",
+            "2h 13m",
+            "2h 14m",
+            "2h 9m",
+            "1h 38m",
+            "2h 29m"
+    )
+}

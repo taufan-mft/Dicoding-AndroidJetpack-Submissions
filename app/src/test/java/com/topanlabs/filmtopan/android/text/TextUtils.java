7c7c9dce5e9e8c43dec6080f65a85302b493d274
Taufan <taufanderazor@gmail.com>
2021-05-04 15:25:27 +0700

    delete unused res

--- app/src/test/java/com/topanlabs/filmtopan/android/text/TextUtils.java
+++
@@ -1,7 +0,0 @@
-package com.topanlabs.filmtopan.android.text;
-
-public class TextUtils {
-    public static boolean isEmpty(CharSequence str) {
-        return str == null || str.length() == 0;
-    }
-}

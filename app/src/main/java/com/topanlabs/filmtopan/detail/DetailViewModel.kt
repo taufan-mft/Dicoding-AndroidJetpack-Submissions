2f5369e726772f312409be3da92283d8f13c9805
Taufan <taufanderazor@gmail.com>
2021-05-04 15:31:47 +0700

    cleanup

--- app/src/main/java/com/topanlabs/filmtopan/detail/DetailViewModel.kt
+++ app/src/main/java/com/topanlabs/filmtopan/detail/DetailViewModel.kt
@@ -2,14 +2,12 @@ package com.topanlabs.filmtopan.detail
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.topanlabs.filmtopan.data.DataRepository
 import com.topanlabs.filmtopan.data.RatingData
 import com.topanlabs.filmtopan.data.RatingFilmData
 import com.topanlabs.filmtopan.utils.EspressoIdlingResource
 import com.topanlabs.filmtopan.utils.Resource
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 /**
@@ -18,17 +16,6 @@ import kotlinx.coroutines.launch
 class DetailViewModel(val repository: DataRepository, val espresso: EspressoIdlingResource) : ViewModel() {
     val selectedFilm = MutableLiveData<Resource<Any>>()
     val selectedTv = MutableLiveData<Resource<Any>>()
-    fun getFilmDetail(movieID: Int) = liveData(Dispatchers.IO) {
-        espresso.increment()
-
-        try {
-            emit(Resource.success(data = repository.getFilmDetail(movieID)))
-        } catch (exception: Exception) {
-            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
-        }
-        espresso.decrement()
-
-    }
 
     fun setFilm(movieID: Int) {
         espresso.increment()
